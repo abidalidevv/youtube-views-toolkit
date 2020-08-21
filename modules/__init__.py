cc6e783b88aece763d6b9d2663e3146d8fe12de0
abidmmp <abidmmp99@gmail.com>
2020-08-21 11:34:00 +0500

    Optimize get_env performance

--- modules/__init__.py
+++ modules/__init__.py
@@ -5,3 +5,8 @@ def slugify(text: str) -> str:
     text = text.lower().strip()
     text = re.sub(r'[^\w\s-]', '', text)
     return re.sub(r'[\s_-]+', '-', text)
+
+
+def get_env(key: str, default: str = '') -> str:
+    import os
+    return os.environ.get(key, default)
