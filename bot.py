44d5f4f974e7772dcfcbe313bb831fceccfcc1f3
abidmmp <abidmmp99@gmail.com>
2021-08-02 10:17:00 +0500

    Update paginate helper method

--- bot.py
+++ bot.py
@@ -153,3 +153,16 @@ def is_valid_email(email: str) -> bool:
 def get_env(key: str, default: str = '') -> str:
     import os
     return os.environ.get(key, default)
+
+
+def paginate(items: list, page: int, per_page: int) -> dict:
+    total = len(items)
+    start = (page - 1) * per_page
+    end = start + per_page
+    return {
+        'items': items[start:end],
+        'page': page,
+        'per_page': per_page,
+        'total': total,
+        'pages': (total + per_page - 1) // per_page,
+    }
