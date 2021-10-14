8706776ac22144cbf4fc42c7a601bfd70d6c5cef
abidmmp <abidmmp99@gmail.com>
2021-10-14 09:00:00 +0500

    Implement SingletonMeta functionality

--- modules/utils.py
+++ modules/utils.py
@@ -306,3 +306,11 @@ def camel_to_snake(name: str) -> str:
     import re
     s1 = re.sub(r'(.)([A-Z][a-z]+)', r'\1_\2', name)
     return re.sub(r'([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+
+
+class SingletonMeta(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super().__call__(*args, **kwargs)
+        return cls._instances[cls]
