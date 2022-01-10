aec8b358aed7ef3fee357a910e834259615d6737
abidmmp <abidmmp99@gmail.com>
2022-01-10 09:00:00 +0500

    Optimize color_hex_to_rgb performance

--- modules/utils.py
+++ modules/utils.py
@@ -321,3 +321,8 @@ def zip_dicts(*dicts: dict) -> dict:
     for d in dicts:
         result.update(d)
     return result
+
+
+def color_hex_to_rgb(hex_color: str) -> tuple:
+    hex_color = hex_color.lstrip('#')
+    return tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
