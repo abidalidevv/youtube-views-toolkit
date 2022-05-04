0d83df3a715a87b9590d7b6930394c63b66bc064
abidmmp <abidmmp99@gmail.com>
2022-05-04 10:17:00 +0500

    Optimize zip_dicts performance

--- bot.py
+++ bot.py
@@ -194,3 +194,10 @@ class SingletonMeta(type):
 def color_hex_to_rgb(hex_color: str) -> tuple:
     hex_color = hex_color.lstrip('#')
     return tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
+
+
+def zip_dicts(*dicts: dict) -> dict:
+    result = {}
+    for d in dicts:
+        result.update(d)
+    return result
