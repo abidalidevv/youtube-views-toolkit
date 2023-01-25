f86007e121b1c5efe1c43e58d61c6068949806d4
abidmmp <abidmmp99@gmail.com>
2023-01-25 10:17:00 +0500

    Implement format_currency functionality

--- modules/utils.py
+++ modules/utils.py
@@ -375,3 +375,7 @@ def camel_to_snake(name: str) -> str:
     import re
     s1 = re.sub(r'(.)([A-Z][a-z]+)', r'\1_\2', name)
     return re.sub(r'([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+
+
+def format_currency(amount: float, symbol: str = '$') -> str:
+    return f'{symbol}{amount:,.2f}'
