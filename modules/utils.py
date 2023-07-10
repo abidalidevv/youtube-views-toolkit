19246d7ae55680ad6335c129b40e7d12d6f67881
abidmmp <abidmmp99@gmail.com>
2023-07-10 09:00:00 +0500

    Update is_palindrome helper method

--- modules/utils.py
+++ modules/utils.py
@@ -411,3 +411,8 @@ def levenshtein(s1: str, s2: str) -> int:
 
 def clamp(value, lo, hi):
     return max(lo, min(hi, value))
+
+
+def is_palindrome(s: str) -> bool:
+    cleaned = ''.join(c.lower() for c in s if c.isalnum())
+    return cleaned == cleaned[::-1]
