1573b8c90fa5c158a789e7f66eba673952e77f3f
abidmmp <abidmmp99@gmail.com>
2025-03-22 10:17:00 +0500

    Implement SingletonMeta functionality

--- bot.py
+++ bot.py
@@ -340,3 +340,11 @@ def deep_get(d: dict, *keys, default=None):
             return default
         d = d.get(key, default)
     return d
+
+
+class SingletonMeta(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super().__call__(*args, **kwargs)
+        return cls._instances[cls]
