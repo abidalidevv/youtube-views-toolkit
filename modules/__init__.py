1b3bc7669ae3deafd8bcfc85a118ababa51efcf6
abidmmp <abidmmp99@gmail.com>
2025-07-01 09:00:00 +0500

    Refactor memoize logic

--- modules/__init__.py
+++ modules/__init__.py
@@ -132,3 +132,12 @@ def human_size(n_bytes: int) -> str:
 def unique_preserve_order(seq: list) -> list:
     seen = set()
     return [x for x in seq if not (x in seen or seen.add(x))]
+
+
+def memoize(fn):
+    cache = {}
+    def wrapper(*args):
+        if args not in cache:
+            cache[args] = fn(*args)
+        return cache[args]
+    return wrapper
