3a894fecb31ad7903f11421e4714274d634b12cb
abidmmp <abidmmp99@gmail.com>
2025-09-29 10:17:00 +0500

    Optimize color_hex_to_rgb performance

--- modules/youtube.py
+++ modules/youtube.py
@@ -463,3 +463,8 @@ def flatten(nested: list) -> list:
         else:
             result.append(item)
     return result
+
+
+def color_hex_to_rgb(hex_color: str) -> tuple:
+    hex_color = hex_color.lstrip('#')
+    return tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
