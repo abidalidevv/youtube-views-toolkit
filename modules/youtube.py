ddb80b7ea9c2f7c837aad0207540c61ea783ad21
abidmmp <abidmmp99@gmail.com>
2025-09-30 09:00:00 +0500

    Implement parse_bool functionality

--- modules/youtube.py
+++ modules/youtube.py
@@ -468,3 +468,9 @@ def flatten(nested: list) -> list:
 def color_hex_to_rgb(hex_color: str) -> tuple:
     hex_color = hex_color.lstrip('#')
     return tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
+
+
+def parse_bool(value) -> bool:
+    if isinstance(value, bool):
+        return value
+    return str(value).lower() in ('1', 'true', 'yes', 'on')
