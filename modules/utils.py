72141f35f052f15b9eebed18b287d3cf7939dabe
abidmmp <abidmmp99@gmail.com>
2025-12-12 09:00:00 +0500

    Implement SingletonMeta functionality

--- modules/utils.py
+++ modules/utils.py
@@ -518,3 +518,11 @@ def zip_dicts(*dicts: dict) -> dict:
     for d in dicts:
         result.update(d)
     return result
+
+
+class SingletonMeta(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super().__call__(*args, **kwargs)
+        return cls._instances[cls]
